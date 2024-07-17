d745d77277fcc3b97918dfd5ab12a426f3864908
FilipyStudy <fiilipydasilvafurtado@gmail.com>
2024-07-16 23:01:28 -0300

    feat: added some code

--- scripts/ml.py
+++ scripts/ml.py
@@ -1,3 +1,13 @@
-import torch
-import nltk
-nltk.download('plunkt')
+import torchtext
+from torchtext.data import get_tokenizer
+import numpy as np
+import pandas as pd
+import db_manager as db
+
+
+class machine_learning:
+
+    def tokenize():
+        connection = db.create_connection()
+        table = pd.read_sql('system_data', connection)
+        
