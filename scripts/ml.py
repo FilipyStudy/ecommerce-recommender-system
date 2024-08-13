10ccd9f0ab0f9d342325505cee9759fe0c02bce3
FilipyStudy <fiilipydasilvafurtado@gmail.com>
2024-08-12 21:41:09 -0300

    fix: created a TODO comment inside the ml.py script.

--- scripts/ml.py
+++ scripts/ml.py
@@ -1,4 +1,4 @@
-import torchtext
+import torchtext; torchtext.disable_torchtext_deprecation_warning()
 from torchtext.data import get_tokenizer
 import numpy as np
 import pandas as pd
@@ -10,4 +10,5 @@ class machine_learning:
     def tokenize():
         connection = db.create_connection()
         table = pd.read_sql('system_data', connection)
-        
+
+#TODO: Create a machine Learning algorithm to enjoy the recommendation system.
