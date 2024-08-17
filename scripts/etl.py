f66998b5c240becb275c1c2d72bc06bb7553d48b
FilipyStudy <fiilipydasilvafurtado@gmail.com>
2024-08-17 20:13:13 -0300

    Add: Added some imports to avoid ponctuation and stop words

--- scripts/etl.py
+++ scripts/etl.py
@@ -2,6 +2,8 @@ import db_manager
 import pandas as pd
 import pathlib
 from sqlalchemy import create_engine
+import re
+import nltk
 
 DB_INFO = {
     "db_name" : "system_data",
@@ -11,6 +13,8 @@ DB_INFO = {
     "port" : 5432
 }
 
+regex_pattern = ["a-z"]
+
 try:
     #Open the database, create a connection and upload the data to a database after the ETL process.
     with open(pathlib.Path("database\\train.csv")) as f:
@@ -19,10 +23,12 @@ try:
     uri = f"postgresql+psycopg://{DB_INFO['user']}:{DB_INFO['password']}@{DB_INFO['db_address']}:{DB_INFO['port']}/{DB_INFO['db_name']}"
     engine = create_engine(uri)
 
-    #Remove incorret values from the first index
+    #Remove incorret values from the first index, stop words and ponctuation characters using regex
     for x in csv_table.index():
         if csv_table.loc[x, 0] != "1" or csv_table.loc[x, 0] != "2":
             csv_table.drop(x, inplace=True, erros="ignore")
+        #TODO: Create a regex function to avoid numbers, pontuations and stop words.
+        
     
     #Upload the data to a postgresql database
     csv_table.to_sql(name="training_data",
