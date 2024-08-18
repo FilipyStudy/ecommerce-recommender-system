e3963df707cf8b8168f4752b539cc70cc260def3
FilipyStudy <fiilipydasilvafurtado@gmail.com>
2024-08-18 01:21:41 -0300

    add: added some code to avoid ponctuation and stop words using re and nltk libraries

--- scripts/etl.py
+++ scripts/etl.py
@@ -4,6 +4,7 @@ import pathlib
 from sqlalchemy import create_engine
 import re
 import nltk
+from nltk.corpus import stopwords
 
 DB_INFO = {
     "db_name" : "system_data",
@@ -13,7 +14,10 @@ DB_INFO = {
     "port" : 5432
 }
 
-regex_pattern = ["a-z"]
+#Create the pattern for regex ETL process
+pattern = re.compile(r"[\u0041-\u1EFF\s]+\s?")
+nltk.download('stopwords')
+stop_words = set(stopwords.words('english'))
 
 try:
     #Open the database, create a connection and upload the data to a database after the ETL process.
@@ -23,12 +27,17 @@ try:
     uri = f"postgresql+psycopg://{DB_INFO['user']}:{DB_INFO['password']}@{DB_INFO['db_address']}:{DB_INFO['port']}/{DB_INFO['db_name']}"
     engine = create_engine(uri)
 
-    #Remove incorret values from the first index, stop words and ponctuation characters using regex
+    #Remove incorret values from the first index, stop words and ponctuation characters using regex and nltk
     for x in csv_table.index():
         if csv_table.loc[x, 0] != "1" or csv_table.loc[x, 0] != "2":
             csv_table.drop(x, inplace=True, erros="ignore")
-        #TODO: Create a regex function to avoid numbers, pontuations and stop words.
+        temp_phrase = "".join(i for i in pattern.findall(csv_table.loc[x, 1]) if i not in stop_words)
+                              
+        temp_phrase_two = "".join(i for i in pattern.findall(csv_table.loc[x, 2]) if i not in stop_words)
         
+        csv_table.loc[x, 1] = temp_phrase
+        
+        csv_table.loc[x, 2] = temp_phrase_two
     
     #Upload the data to a postgresql database
     csv_table.to_sql(name="training_data",
